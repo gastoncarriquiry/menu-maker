ccbebf1638f8a09b73b39646115d1164585873ad
carriquiry-g <carriquiry@ort.edu.uy>
2025-06-24 21:37:19 -0300

    feat: update ESLint configuration to use TypeScript parser and plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,23 @@
-import { defineConfig } from 'eslint/config';
+import typescript from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
 
-export default defineConfig([
+export default [
   {
     files: ['**/*.ts', '**/*.js', '**/*.mjs'],
+    languageOptions: {
+      parser: parser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+    },
     rules: {
       semi: ['error', 'always'],
       'prefer-const': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
-]);
+];
